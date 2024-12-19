4901f9b0473eb7ba2491bc55be9d2759dd2029da
divineProjects <divinejigu@gmail.com>
2024-12-19 07:29:36 +0200

    jest test

---
+++ controllers/getStudents.test.js
@@ -0,0 +1,70 @@
+const { getAll } = require('./studentsController');
+const mongodb = require('../data/database'); 
+const { ObjectId } = require('mongodb');
+
+const mockStudents = [
+    {
+        _id: new ObjectId(),
+        "name": "John Doe",
+        "age": 20,
+        "email": "johndoe@example.com",
+        "classes": [
+            "CS101",
+            "PHY101"
+        ],
+        "grades": [
+            "GRADE001",
+            "GRADE004"
+        ],
+        "clubs": [
+            "CLUB001"
+        ],
+        "enrollmentDate": "2023-08-25T00:00:00Z"
+    },
+    {
+        _id: new ObjectId(),
+        "name": "Jane Smith",
+        "age": 22,
+        "email": "janesmith@example.com",
+        "classes": [
+            "MATH204",
+            "ENG150"
+        ],
+        "grades": [
+            "GRADE002"
+        ],
+        "clubs": [],
+        "enrollmentDate": "2023-08-26T00:00:00Z"
+    },
+];
+
+describe("getAll Students Controller", () => {
+    let req, res;
+
+    beforeEach(() => {
+        req = {};
+        res = {
+            setHeader: jest.fn(),
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        const collectionMock = {
+            find: jest.fn().mockReturnValue({
+                toArray: jest.fn().mockResolvedValue(mockStudents),
+            }),
+        };
+        mongodb.getDatabase = jest.fn().mockReturnValue({
+            collection: jest.fn().mockReturnValue(collectionMock),
+        });
+    });
+
+    it("should respond with a 200 status and return all students", async () => {
+        await getAll(req, res);
+
+        expect(mongodb.getDatabase).toHaveBeenCalled();
+        expect(res.setHeader).toHaveBeenCalledWith('Content-Type', 'application/json');
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith(mockStudents);
+    });
+});
